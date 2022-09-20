aba05336a3feeb8e461a2d8301951095527ea673
Azubire Peter <azubirepeter@gmail.com>
2022-09-20 19:38:14 +0000

    -

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,9 @@ const HomeController = require("../controllers/HomeController");
 const UserController = require("../controllers/UserController");
 
 // routes
+router.get("/", (req, res) => {
+  res.end("success");
+});
 router.get("/home/categories", HomeController.getHomeCategories);
 router.get("/user/:id", UserController.getUser);
 router.get("/dashboard", UserController.getMetrics);
